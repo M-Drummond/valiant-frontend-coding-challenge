18bdbf07822600ca39c5b82aeec010432e0b1996
Mackinley <mackinley@punchbuggy.com.au>
2024-04-18 17:27:08 +1000

    base input component css

--- src/components/LoanInputAmount.vue
+++ src/components/LoanInputAmount.vue
@@ -10,10 +10,20 @@ defineProps({
 </script>
 
 <template>
-  <input
-    placeholder="$30 000"
-    type="number"
-    pattern="[0-9]*"
-  >
+  <div class="flex flex-row items-center justify-center">
+    <label
+      for="LoanAmount"
+      class="mr-2 shrink-0"
+    >
+      I need
+    </label>
+    <input
+      id="LoanAmount"
+      placeholder="$30 000"
+      type="number"
+      pattern="[0-9]*"
+      class="form-input"
+    >
+  </div>
 </template>
 <style scoped></style>
